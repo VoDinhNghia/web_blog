d36a45bf5c5b33801b5c05c24a2b1c7ccc371c2e
Võ Đình Nghĩa <56909768+VoDinhNghia@users.noreply.github.com>
2021-08-22 11:26:38 +0700

    v1
    v1

--- app/config/Config.js
+++ app/config/Config.js
@@ -12,7 +12,7 @@ exports.ConfigMail = {
     Email: 'sciencepost95@gmail.com',
     PassWord: '*******'
 }
-
+//check nếu user nhập những từ thô tục v
 exports.checkContent = ['địt mẹ', 'địt má', 'đụ', 'lồn', 'Lồn', 'cái bím', 'Cái bím',
     'Cặt', 'cặt', 'Địt má', 'fuck', 'Đụ', 'Địt mẹ', 'Lịt mẹ', 'lịt mẹ', 'Lịt má', 'lịt má',
     'lịt mọe', 'Lịt mọe', 'Địt mày', 'địt mày', 'Ụ má', 'Ụ mẹ'
